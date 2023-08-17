00b2c4e07a1d058bf7491888bec0bb77f73396f9
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:34:06 +0200

    ex3

---
+++ ex3/Operaciones.cs
@@ -0,0 +1,44 @@
+﻿// Operaciones.cs
+using System;
+
+namespace ex3
+{
+    class Operaciones
+    {
+        private int num1;
+        private int num2;
+
+        public Operaciones(int num1, int num2)
+        {
+            this.num1 = num1;
+            this.num2 = num2;
+        }
+
+        public int Suma()
+        {
+            return num1 + num2;
+        }
+
+        public int Resta()
+        {
+            return num1 - num2;
+        }
+
+        public int Multiplicacion()
+        {
+            return num1 * num2;
+        }
+
+        public double Division()
+        {
+            if (num2 != 0)
+            {
+                return (double)num1 / num2;
+            }
+            else
+            {
+                throw new DivideByZeroException("No se puede dividir por cero.");
+            }
+        }
+    }
+}
---
+++ ex3/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Operaciones operaciones = new Operaciones(2, 3);
+            Console.WriteLine(operaciones.Suma());
+            Console.WriteLine(operaciones.Resta());
+            Console.WriteLine(operaciones.Multiplicacion());
+            Console.WriteLine(operaciones.Division());
+        }
+    }
+}
