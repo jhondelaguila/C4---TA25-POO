c1ab48816b5b06e85fb43ca8a4f1f3d2e2e23069
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 16:36:11 +0200

    ex9

---
+++ ex9/Electrodomestico.cs
@@ -0,0 +1,94 @@
+﻿using System;
+
+namespace ex9
+{
+    class Electrodomestico
+    {
+        protected double precioBase;
+        protected string color;
+        protected char consumoEnergetico;
+        protected double peso;
+
+        public const string ColorBlanco = "blanco";
+        public const string ColorNegro = "negro";
+        public const string ColorRojo = "rojo";
+        public const string ColorAzul = "azul";
+        public const string ColorGris = "gris";
+
+        public const char ConsumoEnergeticoDefault = 'F';
+
+        public Electrodomestico()
+        {
+            precioBase = 100;
+            color = ColorBlanco;
+            consumoEnergetico = ConsumoEnergeticoDefault;
+            peso = 5;
+        }
+
+        public Electrodomestico(double precioBase, double peso)
+        {
+            this.precioBase = precioBase;
+            color = ColorBlanco;
+            consumoEnergetico = ConsumoEnergeticoDefault;
+            this.peso = peso;
+        }
+
+        public Electrodomestico(double precioBase, string color, char consumoEnergetico, double peso)
+        {
+            this.precioBase = precioBase;
+            EstablecerColor(color);
+            EstablecerConsumoEnergetico(consumoEnergetico);
+            this.peso = peso;
+        }
+
+        private void EstablecerColor(string color)
+        {
+            string colorLower = color.ToLower();
+            if (colorLower == ColorBlanco || colorLower == ColorNegro ||
+                colorLower == ColorRojo || colorLower == ColorAzul || colorLower == ColorGris)
+            {
+                this.color = colorLower;
+            }
+            else
+            {
+                this.color = ColorBlanco; // Valor por defecto si el color no es válido
+            }
+        }
+
+        private void EstablecerConsumoEnergetico(char consumoEnergetico)
+        {
+            if (consumoEnergetico >= 'A' && consumoEnergetico <= 'F')
+            {
+                this.consumoEnergetico = consumoEnergetico;
+            }
+            else
+            {
+                this.consumoEnergetico = ConsumoEnergeticoDefault; // Valor por defecto si el consumo energético no es válido
+            }
+        }
+
+        public double PrecioBase
+        {
+            get { return precioBase; }
+            set { precioBase = value; }
+        }
+
+        public string Color
+        {
+            get { return color; }
+            set { EstablecerColor(value); }
+        }
+
+        public char ConsumoEnergetico
+        {
+            get { return consumoEnergetico; }
+            set { EstablecerConsumoEnergetico(value); }
+        }
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+    }
+}
---
+++ ex9/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace ex9
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Electrodomestico electrodomestico1 = new Electrodomestico();
+            Console.WriteLine("Electrodomestico 1:");
+            Console.WriteLine($"Precio Base: {electrodomestico1.PrecioBase}");
+            Console.WriteLine($"Color: {electrodomestico1.Color}");
+            Console.WriteLine($"Consumo Energético: {electrodomestico1.ConsumoEnergetico}");
+            Console.WriteLine($"Peso: {electrodomestico1.Peso}");
+            Console.WriteLine();
+
+            Electrodomestico electrodomestico2 = new Electrodomestico(200, 10);
+            Console.WriteLine("Electrodomestico 2:");
+            Console.WriteLine($"Precio Base: {electrodomestico2.PrecioBase}");
+            Console.WriteLine($"Color: {electrodomestico2.Color}");
+            Console.WriteLine($"Consumo Energético: {electrodomestico2.ConsumoEnergetico}");
+            Console.WriteLine($"Peso: {electrodomestico2.Peso}");
+            Console.WriteLine();
+
+            Electrodomestico electrodomestico3 = new Electrodomestico(300, "negro", 'A', 15);
+            Console.WriteLine("Electrodomestico 3:");
+            Console.WriteLine($"Precio Base: {electrodomestico3.PrecioBase}");
+            Console.WriteLine($"Color: {electrodomestico3.Color}");
+            Console.WriteLine($"Consumo Energético: {electrodomestico3.ConsumoEnergetico}");
+            Console.WriteLine($"Peso: {electrodomestico3.Peso}");
+        }
+    }
+}
