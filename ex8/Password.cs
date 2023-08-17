f1ebdff74fc49c74793c6eaa0bb399b1e87a2792
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:58:33 +0200

    ex8

---
+++ ex8/Password.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace ex8
+{
+    class Password
+    {
+        private int longitud;
+        private string contraseña;
+
+        public int Longitud
+        {
+            get { return longitud; }
+        }
+
+        public string Contraseña
+        {
+            get { return contraseña; }
+        }
+
+        public Password()
+        {
+            longitud = 8;
+            GenerarContraseñaAleatoria();
+        }
+
+        public Password(int longitud)
+        {
+            this.longitud = longitud;
+            GenerarContraseñaAleatoria();
+        }
+
+        private void GenerarContraseñaAleatoria()
+        {
+            const string caracteres = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+            Random random = new Random();
+            char[] contraseñaArray = new char[longitud];
+
+            for (int i = 0; i < longitud; i++)
+            {
+                contraseñaArray[i] = caracteres[random.Next(caracteres.Length)];
+            }
+
+            contraseña = new string(contraseñaArray);
+        }
+    }
+}
---
+++ ex8/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace ex8
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Password contraseña1 = new Password();
+            Console.WriteLine($"Contraseña 1 (longitud {contraseña1.Longitud}): {contraseña1.Contraseña}");
+
+            Password contraseña2 = new Password(12);
+            Console.WriteLine($"Contraseña 2 (longitud {contraseña2.Longitud}): {contraseña2.Contraseña}");
+        }
+    }
+}
