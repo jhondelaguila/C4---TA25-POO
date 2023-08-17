99328d9311fd6d0c5316fee0296638fb2df8f5ea
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:23:24 +0200

    ex1

---
+++ ex1/Alumno.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace ex1
+{
+	public class Alumno
+	{
+		private string nombre;
+		private int edad;
+
+		public Alumno(string nombre, int edad)
+		{
+			this.nombre = nombre;
+			this.edad = edad;
+		}
+
+		override
+		public string ToString()
+		{
+			return $"nombre: {nombre}, edad: {edad}";
+		}
+
+		public void mayorEdad()
+		{
+			if (this.edad >= 18) Console.WriteLine("Es mayor de edad");
+			else Console.WriteLine("No es mayor de edad");
+        }
+	}
+}
+
---
+++ ex1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Alumno alumno = new Alumno("Jhon", 30);
+            Console.WriteLine(alumno.ToString());
+            alumno.mayorEdad();
+        }
+    }
+}
