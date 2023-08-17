aaf4c7686185d1650e91cc666938b24438b2d2a2
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:47:57 +0200

    ex6

---
+++ ex6/Coche.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex6
+{
+    class Coche
+    {
+        private string marca;
+        private string modelo;
+        private int cilindrada;
+        private double potencia;
+
+        public Coche() { }
+
+        public string Marca
+        {
+            get { return marca; }
+            set { marca = value; }
+        }
+
+        public string Modelo
+        {
+            get { return modelo; }
+            set { modelo = value; }
+        }
+
+        public int Cilindrada
+        {
+            get { return cilindrada; }
+            set { cilindrada = value; }
+        }
+
+        public double Potencia
+        {
+            get { return potencia; }
+            set { potencia = value; }
+        }
+    }
+}
---
+++ ex6/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace ex6
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coche miCoche = new Coche();
+
+            miCoche.Marca = "Toyota";
+            miCoche.Modelo = "Corolla";
+            miCoche.Cilindrada = 2000;
+            miCoche.Potencia = 150.5;
+
+            Console.WriteLine("Detalles del coche:");
+            Console.WriteLine($"Marca: {miCoche.Marca}");
+            Console.WriteLine($"Modelo: {miCoche.Modelo}");
+            Console.WriteLine($"Cilindrada: {miCoche.Cilindrada} cc");
+            Console.WriteLine($"Potencia: {miCoche.Potencia} HP");
+        }
+    }
+}
