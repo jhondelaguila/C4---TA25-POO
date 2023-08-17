e0ba2ed4915f78a2130d5c3b2dd6a5f1af2ea1f0
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:39:41 +0200

    ex4

---
+++ ex4/Persona.cs
@@ -0,0 +1,22 @@
+﻿// Persona.cs
+using System;
+
+namespace ex4
+{
+    class Persona
+    {
+        private string nombre;
+
+        public Persona() { }
+
+        public void SetNombre(string nuevoNombre)
+        {
+            this.nombre = nuevoNombre;
+        }
+
+        public void Saludar()
+        {
+            Console.WriteLine($"Hola, soy {nombre}");
+        }
+    }
+}
---
+++ ex4/PruebaPersona.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ex4
+{
+	public class PruebaPersona
+	{
+		static void Main(string[] args)
+		{
+			Persona persona = new Persona();
+			persona.SetNombre("Jhon");
+			persona.Saludar();
+		}
+	}
+}
+
