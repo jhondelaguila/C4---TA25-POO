0706f4a9190b696f7277ace48940397bbece000d
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:43:59 +0200

    ex5

---
+++ ex5/Libro.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace ex5
+{
+    class Libro
+    {
+        private string autor;
+        private string titulo;
+        private string ubicacion;
+
+        public Libro() { }
+
+        public string GetAutor()
+        {
+            return this.autor;
+        }
+
+        public void SetAutor(string nuevoAutor)
+        {
+            this.autor = nuevoAutor;
+        }
+
+        public string GetTitulo()
+        {
+            return this.titulo;
+        }
+
+        public void SetTitulo(string nuevoTitulo)
+        {
+            this.titulo = nuevoTitulo;
+        }
+
+        public string GetUbicacion()
+        {
+            return this.ubicacion;
+        }
+
+        public void SetUbicacion(string nuevaUbicacion)
+        {
+            this.ubicacion = nuevaUbicacion;
+        }
+    }
+}
---
+++ ex5/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace ex5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Libro libro = new Libro();
+
+            Console.Write("Ingrese el autor del libro: ");
+            string autor = Console.ReadLine();
+            libro.SetAutor(autor);
+
+            Console.Write("Ingrese el título del libro: ");
+            string titulo = Console.ReadLine();
+            libro.SetTitulo(titulo);
+
+            Console.Write("Ingrese la ubicación del libro: ");
+            string ubicacion = Console.ReadLine();
+            libro.SetUbicacion(ubicacion);
+
+            Console.WriteLine();
+            Console.WriteLine("Detalles del libro:");
+            Console.WriteLine($"Autor: {libro.GetAutor()}");
+            Console.WriteLine($"Título: {libro.GetTitulo()}");
+            Console.WriteLine($"Ubicación: {libro.GetUbicacion()}");
+        }
+    }
+}
