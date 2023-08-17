85a6d8c079f8a5582e0b778f669069fb0770b03d
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:56:12 +0200

    ex7

---
+++ ex7/Persona.cs
@@ -0,0 +1,81 @@
+﻿using System;
+
+namespace ex7
+{
+    class Persona
+    {
+        private string nombre;
+        private int edad;
+        private string DNI;
+        private char sexo;
+        private double peso;
+        private double altura;
+
+        public const char SexoHombre = 'H';
+
+        public Persona(string dni)
+        {
+            nombre = "";
+            edad = 0;
+            DNI = dni;
+            sexo = SexoHombre;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string dni, string nombre, int edad)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            DNI = dni;
+            sexo = SexoHombre;
+            peso = 0;
+            altura = 0;
+        }
+
+        public Persona(string dni, string nombre, int edad, double peso, double altura)
+        {
+            this.nombre = nombre;
+            this.edad = edad;
+            DNI = dni;
+            sexo = SexoHombre;
+            this.peso = peso;
+            this.altura = altura;
+        }
+
+        public string Nombre
+        {
+            get { return nombre; }
+            set { nombre = value; }
+        }
+
+        public int Edad
+        {
+            get { return edad; }
+            set { edad = value; }
+        }
+
+        public string Dni
+        {
+            get { return DNI; }
+        }
+
+        public char Sexo
+        {
+            get { return sexo; }
+            set { sexo = value; }
+        }
+
+        public double Peso
+        {
+            get { return peso; }
+            set { peso = value; }
+        }
+
+        public double Altura
+        {
+            get { return altura; }
+            set { altura = value; }
+        }
+    }
+}
---
+++ ex7/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ex7
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Persona persona1 = new Persona("12345678A");
+            Console.WriteLine("Persona 1:");
+            Console.WriteLine($"Nombre: {persona1.Nombre}");
+            Console.WriteLine($"Edad: {persona1.Edad}");
+            Console.WriteLine($"DNI: {persona1.Dni}");
+            Console.WriteLine($"Sexo: {persona1.Sexo}");
+            Console.WriteLine($"Peso: {persona1.Peso}");
+            Console.WriteLine($"Altura: {persona1.Altura}");
+            Console.WriteLine();
+
+            Persona persona2 = new Persona("12345678A","Juan", 30);
+            Console.WriteLine("Persona 2:");
+            Console.WriteLine($"Nombre: {persona2.Nombre}");
+            Console.WriteLine($"Edad: {persona2.Edad}");
+            Console.WriteLine($"DNI: {persona2.Dni}");
+            Console.WriteLine($"Sexo: {persona2.Sexo}");
+            Console.WriteLine($"Peso: {persona2.Peso}");
+            Console.WriteLine($"Altura: {persona2.Altura}");
+            Console.WriteLine();
+
+            Persona persona3 = new Persona("12345678A","María", 25, 55.5, 1.65);
+            Console.WriteLine("Persona 3:");
+            Console.WriteLine($"Nombre: {persona3.Nombre}");
+            Console.WriteLine($"Edad: {persona3.Edad}");
+            Console.WriteLine($"DNI: {persona3.Dni}");
+            Console.WriteLine($"Sexo: {persona3.Sexo}");
+            Console.WriteLine($"Peso: {persona3.Peso}");
+            Console.WriteLine($"Altura: {persona3.Altura}");
+        }
+    }
+}
