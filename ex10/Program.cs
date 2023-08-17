a311038d999cff4306b812567a9afd2c3eed78f1
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 16:38:23 +0200

    ex10

---
+++ ex10/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+
+namespace ex10
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Serie serie1 = new Serie();
+            Console.WriteLine("Serie 1:");
+            Console.WriteLine($"Título: {serie1.Titulo}");
+            Console.WriteLine($"Número de Temporadas: {serie1.NumeroTemporadas}");
+            Console.WriteLine($"Entregado: {serie1.Entregado}");
+            Console.WriteLine($"Género: {serie1.Genero}");
+            Console.WriteLine($"Creador: {serie1.Creador}");
+            Console.WriteLine();
+
+            Serie serie2 = new Serie("Game of Thrones", "George R. R. Martin");
+            Console.WriteLine("Serie 2:");
+            Console.WriteLine($"Título: {serie2.Titulo}");
+            Console.WriteLine($"Número de Temporadas: {serie2.NumeroTemporadas}");
+            Console.WriteLine($"Entregado: {serie2.Entregado}");
+            Console.WriteLine($"Género: {serie2.Genero}");
+            Console.WriteLine($"Creador: {serie2.Creador}");
+            Console.WriteLine();
+
+            Serie serie3 = new Serie("Stranger Things", 5, "Ciencia Ficción", "The Duffer Brothers");
+            Console.WriteLine("Serie 3:");
+            Console.WriteLine($"Título: {serie3.Titulo}");
+            Console.WriteLine($"Número de Temporadas: {serie3.NumeroTemporadas}");
+            Console.WriteLine($"Entregado: {serie3.Entregado}");
+            Console.WriteLine($"Género: {serie3.Genero}");
+            Console.WriteLine($"Creador: {serie3.Creador}");
+        }
+    }
+}
---
+++ ex10/Serie.cs
@@ -0,0 +1,73 @@
+﻿using System;
+
+namespace ex10
+{
+    class Serie
+    {
+        private string titulo;
+        private int numeroTemporadas;
+        private bool entregado;
+        private string genero;
+        private string creador;
+
+        public const int NumeroTemporadasDefault = 3;
+        public const bool EntregadoDefault = false;
+
+        public string Titulo
+        {
+            get { return titulo; }
+            set { titulo = value; }
+        }
+
+        public int NumeroTemporadas
+        {
+            get { return numeroTemporadas; }
+            set { numeroTemporadas = value; }
+        }
+
+        public bool Entregado
+        {
+            get { return entregado; }
+            set { entregado = value; }
+        }
+
+        public string Genero
+        {
+            get { return genero; }
+            set { genero = value; }
+        }
+
+        public string Creador
+        {
+            get { return creador; }
+            set { creador = value; }
+        }
+
+        public Serie()
+        {
+            titulo = "";
+            numeroTemporadas = NumeroTemporadasDefault;
+            entregado = EntregadoDefault;
+            genero = "";
+            creador = "";
+        }
+
+        public Serie(string titulo, string creador)
+        {
+            this.titulo = titulo;
+            this.creador = creador;
+            numeroTemporadas = NumeroTemporadasDefault;
+            entregado = EntregadoDefault;
+            genero = "";
+        }
+
+        public Serie(string titulo, int numeroTemporadas, string genero, string creador)
+        {
+            this.titulo = titulo;
+            this.numeroTemporadas = numeroTemporadas;
+            this.genero = genero;
+            this.creador = creador;
+            entregado = EntregadoDefault;
+        }
+    }
+}
