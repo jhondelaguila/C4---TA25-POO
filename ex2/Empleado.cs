ebd03b4d180bacf35d959b69b9c6ad8fa8834726
Jhon Junior del Águila Pinedo <jhonjr.dap@gmail.com>
2023-08-17 15:30:35 +0200

    ex2

---
+++ ex2/Empleado.cs
@@ -0,0 +1,35 @@
+﻿// Empleado.cs
+using System;
+
+namespace ex2
+{
+    class Empleado
+    {
+        private string nombre;
+        private double sueldo;
+
+        public Empleado(string nombre, double sueldo)
+        {
+            this.nombre = nombre;
+            this.sueldo = sueldo;
+        }
+
+        override
+        public string ToString()
+        {
+            return $"Nombre: {nombre},sueldo: {sueldo:C}";
+        }
+
+        public void PagarImpuestos()
+        {
+            if (sueldo > 3000)
+            {
+                Console.WriteLine("Debe pagar impuestos.");
+            }
+            else
+            {
+                Console.WriteLine("No debe pagar impuestos.");
+            }
+        }
+    }
+}
---
+++ ex2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Empleado empleado = new Empleado("Jhon", 3000);
+            Console.WriteLine(empleado.ToString());
+            empleado.PagarImpuestos();
+        }
+    }
+}
